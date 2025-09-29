de64cd272558f81bab7fe824572fcb3f0be30441
Mitles16 <milescutting016@gmail.com>
2025-09-29 18:15:56 +1000

    29 / 9 / 2025
    Began Early Design of the UI and how some of the backend elements will come together. I am making a deliberate decision to make things more modular - so if / when implementing a GUI - in possibly later sieges - development will be simpler.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+Job_Number = 10934
 job = [
         {
         'Order_Number': 10932,
         'Name': 'Jane Smith', 
+        'Contact': {
+            'Phone': '04000000000',
+            'Email': 'a_cool_guy@yahoo.com',
+            'Address': '104 Cool st, Old York, Merica'
+        },
         'Vechicle': ['Suzuki', 'GS500', '2012'],
         'Work_Performed': [{'Work': 'Work', 'Hours_Spent': 'Hours'}],
         'Work_to_Perform': [{'Work': 'Work', 'Estimated_Hours': 'Hours'}],
@@ -9,14 +15,76 @@ job = [
         }
 ]
 
-def display(job, number):
-    print(f'''
+def display(job):
+    for number in range(len(job)):
+        print(f'''
 {'-'*78}
 Order Number:   {job[number]['Order_Number']}
 Name:           {job[number]['Name']}
-Vehicle:        {job[number]['Vechicle'][0]} {job[number]['Vechicle'][1]} {job[number]['Vechicle'][2]}
+Vechicle:       {job[number]['Vechicle'][0]} {job[number]['Vechicle'][1]} {job[number]['Vechicle'][2]}
 {'-'*78}
 ''')
 
+def add_job(new_job):
+    job.append(new_job)
 
-display(job, 0)
+def Create_Job():
+    Job_Number = 10934
+
+    print('Click Enter to Leave Blank')
+    Name = input('Customer Name: ')
+    Contact_Ph = input('Phone Number: ')
+    Contact_Email = input('Email: ')
+    Contact_Address = input('Address: ')
+
+    Vechile_Make = input('Make: ')
+    Vechicle_Model = input('Model: ')
+    Vechicle_Year = input('Year: ')
+
+
+    Job_Number += 1
+
+    new_job = {
+        'Order_Number': Job_Number,
+        'Name': Name, 
+        'Contact': {
+            'Phone': Contact_Ph,
+            'Email': Contact_Email,
+            'Address': Contact_Address
+        },
+        'Vechicle': [Vechile_Make, Vechicle_Model, Vechicle_Year],
+        'Work_Performed': [],
+        'Work_to_Perform': [],
+        'Notes': []
+        }
+    
+    add_job(new_job)
+
+
+def Titlescreen():
+    print(f'''
+{'-'*78}
+1) View All Jobs
+2) View Specific Job
+3) Add New Job
+{'-'*78}
+''')
+    while True:
+        try:
+            choice = int(input('Choice: '))
+
+            if 0 < choice < 5:
+                break
+            else:
+                continue
+        
+        except:
+            pass
+
+    
+            
+
+        
+Create_Job()
+
+display(job)
