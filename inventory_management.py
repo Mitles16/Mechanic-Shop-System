f96fff1dc62e17c691c84b39dffa9a7198063a58
Mitles16 <milescutting016@gmail.com>
2025-10-01 19:44:51 +1000

    1 / 10 / 2025
    The search functionality isnt working correctly - the plan is going to be to remake it completely better using for loops.

--- inventory_management.py
+++ inventory_management.py
@@ -2,28 +2,14 @@
 import json
 
 # --- Backend System ---
-
-# Mock Data
-Parts = [
-{'Part_Number': 'XYZ-ABC-FGH',
- 'Fits': [['Make', 'Model', 'Year'], ['Make', 'Model', 'Year']],
- 'Quantity': 4,
- 'Location': 'Aisle 5, Bay 4'
- }
-]
-
-with open("parts.json", "w") as f:
-    json.dump(Parts, f, indent=4)
-
-
-def Search_Parts(Searching=[], Search_List=None):
+def Search_Parts(Searching, Search_List=None):
     
     Part_Number = Searching[0]
     Make = Searching[1]
     Model = Searching[2]
     Year = Searching[3]
 
-    params = sum(par is not None for par in [Part_Number, Make, Model, Year])
+    params = sum(par != 'None' for par in [Part_Number, Make, Model, Year])
     Searched = []
 
     if Search_List == None:
@@ -33,37 +19,57 @@ def Search_Parts(Searching=[], Search_List=None):
     else:
         Parts = Search_List
 
+    if sum(par != 'None' for par in [Part_Number, Make, Model, Year]) == 4:
+        return Parts
+    
     if params == 1:
-        if Part_Number != None:
+        if Part_Number != 'None':
             for i in list(Parts):
                 if i['Part_Number'] == Part_Number:
                     Searched.append(i)
                 
-        elif Make != None:
+        elif Make != 'None':
             for i in list(Parts):
                 for e in list(i['Fits']):
                     if e[0] == Make:
                         Searched.append(i)
                     
-        elif Model != None:
+        elif Model != 'None':
             for i in list(Parts):
                 for e in list(i['Fits']):
                     if e[1] == Model:
                         Searched.append(i)
                     
-        elif Year != None:
+        elif Year != 'None':
             for i in list(Parts):
                 for e in list(i['Fits']):
                     if e[2] == Year:
                         Searched.append(i)
+
+        
     
+        
     elif params > 0:
-        Search_Parts(Year, Search_Parts(Model, Search_Parts(Make, Search_Parts(Part_Number))))
-            
+        Searched = Search_Parts(['None', Make, 'None', 'None'],
+                    Search_Parts(['None', 'None', Model, 'None'],
+                        Search_Parts(['None', 'None', 'None', Year],
+                            Search_Parts([Part_Number, 'None', 'None', 'None'], Parts))))            
+    
     return Searched
 
-# --- Frontend System ---
 
+
+# --- Frontend System ---
+def Search():
+    Parameters = []
+    while True:
+        for e in list(['Part_Number', 'Make', 'Model', 'Year']):
+            k = input(f'{e}: ')
+            if k == '':
+                k = 'None'
+            Parameters.append(k)
+
+        print(Search_Parts(Parameters))
 
 # --- Test Cases ---
-print(Search_Parts(Part_Number='XYZ-ABC-FGH'))
+Search()
