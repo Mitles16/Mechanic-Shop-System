aea1b8611cddda7703b111ad9902b559ba236908
Mitles16 <milescutting016@gmail.com>
2025-10-06 06:02:25 +1100

    6 / 10 / 2025
    Added my project to PyPi, updated everything, will now ship.

---
+++ package/__init__.py
@@ -0,0 +1,5 @@
+from .main import Add_Inventory
+from .job_management import Search_Jobs
+from .inventory_management import Search_Parts
+
+__all__ = ["Add_Inventory", "Search_Jobs", "Search_Parts"]
